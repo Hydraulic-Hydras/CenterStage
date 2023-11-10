e1e94b4ae28dc9ae0f74b211e2b30b9034228df3
Tea <mtauha9@gmail.com>
2023-11-09 23:24:50 -0500

    Meep Meep

--- MeepMeep/src/main/java/com/hydraulichydras/meepmeep/Left.java
+++ MeepMeep/src/main/java/com/hydraulichydras/meepmeep/Left.java
@@ -6,8 +6,6 @@ import com.noahbres.meepmeep.MeepMeep;
 import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
 import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
 
-import sun.security.provider.JavaKeyStore;
-
 public class Left {
 
     public static void main(String[] args) {
@@ -15,13 +13,13 @@ public class Left {
 
         RoadRunnerBotEntity left = new DefaultBotBuilder(meepMeep)
                 // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
-                .setConstraints(74, 74, Math.toRadians(180), Math.toRadians(180), 15)
+                .setConstraints(74, 74, Math.toRadians(180), Math.toRadians(180), 16.5)
                 .setDimensions(17.5, 17.5)
                 .followTrajectorySequence(drive ->
                         drive.trajectorySequenceBuilder(new Pose2d(-35, -62.5, Math.toRadians(90)))
 
                                 /* === LEFT === */
-
+                                // Team prop on Left
                                 .lineToLinearHeading(new Pose2d(-35.00, -32.00, Math.toRadians(180.00)))
                                 .waitSeconds(1)
 
@@ -36,9 +34,23 @@ public class Left {
                                 .splineTo(new Vector2d(48,-29), Math.toRadians(0))
                                 .waitSeconds(1)
 
+                                .setReversed(false)
+                                .splineToConstantHeading(new Vector2d(20, -11.60), Math.toRadians(180))
+                                .lineTo(new Vector2d(-59,-11.6))
+                                .waitSeconds(1)
+
+                                // repeat this for cycle and travel
+
+                                .setReversed(true)
+                                .lineTo(new Vector2d(20,-11.60))
+                                .waitSeconds(1)
+                                .splineToConstantHeading(new Vector2d(48,-29), Math.toRadians(0))
+                                .waitSeconds(1)
+
                                 .build()
 
                 );
+
         meepMeep.setBackground(MeepMeep.Background.FIELD_CENTERSTAGE_JUICE_DARK)
                 .setDarkMode(true)
                 .setBackgroundAlpha(0.95f)
--- MeepMeep/src/main/java/com/hydraulichydras/meepmeep/Right.java
+++ MeepMeep/src/main/java/com/hydraulichydras/meepmeep/Right.java
@@ -13,106 +13,35 @@ public class Right {
 
         RoadRunnerBotEntity blueRight = new DefaultBotBuilder(meepMeep)
                 // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
-                .setConstraints(74, 74, Math.toRadians(180), Math.toRadians(180), 15)
+                .setConstraints(74, 74, Math.toRadians(180), Math.toRadians(180), 16.5)
                 .setDimensions(17.5,17.5)
                 .followTrajectorySequence(drive ->
-                        drive.trajectorySequenceBuilder(new Pose2d(-35, -62.5, Math.toRadians(90)))
+                        drive.trajectorySequenceBuilder(new Pose2d(12, -62.5, Math.toRadians(90)))
 
+                                /* === RIGHT === */
 
-                                // BLUE RIGHT (RIGHT PROP)
-
-                                .strafeRight(10.7)
-
-                                .lineTo(new Vector2d(-46.7, 35.3))
-
-                                .setTurnConstraint(50,50)
-                                .turn(Math.PI/-2)
-                                .resetTurnConstraint()
-
-                                .lineTo(new Vector2d(-58.3, 35.3))
+                                .lineToLinearHeading(new Pose2d(12, -29, Math.toRadians(180)))
                                 .waitSeconds(1)
 
                                 .setReversed(true)
-                                .splineToSplineHeading(new Pose2d(-40, 12, Math.toRadians(180)), Math.toRadians(0))
-                                .lineTo(new Vector2d(24,12))
-                                .splineToConstantHeading(new Vector2d(50, 41), Math.toRadians(0))
-
-                                .waitSeconds(5)
-
-                                // BLUE RIGHT (CENTER PROP)
-                                /**
-                                .lineTo(new Vector2d(-36, 24))
-
-                                .lineToLinearHeading(new Pose2d(-59, 24, Math.toRadians(180)))
+                                .lineTo(new Vector2d(48, -29))
                                 .waitSeconds(1)
+
+                                .setReversed(false)
+                                .splineToConstantHeading(new Vector2d(35, -58.5), Math.toRadians(180))
+                                .lineTo(new Vector2d(-35, -58.5))
+                                .splineToConstantHeading(new Vector2d(-58, -35.60), Math.toRadians(180))
+                                .waitSeconds(1)
+
                                 .setReversed(true)
-
-                                .splineToSplineHeading(new Pose2d(-40, 12, Math.toRadians(180)), Math.toRadians(0))
-
-                                .lineTo(new Vector2d(24,12))
-
-                                // .lineToLinearHeading(new Pose2d(24, 12, Math.toRadians(0)))
-
-                                //  .splineTo(new Vector2d(50, 35), Math.toRadians(0)) makes movement slower
-
-                                .splineToConstantHeading(new Vector2d(50, 35), Math.toRadians(0))
-                                .waitSeconds(5)
-
-                                **/
 
                                 .build()
-
                 );
-        /*
-        RoadRunnerBotEntity blueLeft = new DefaultBotBuilder(meepMeep)
-                .setColorScheme(new ColorSchemeBlueLight())
-                .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
-                .setDimensions(17.5,17.5)
-                .followTrajectorySequence(drive ->
-                        drive.trajectorySequenceBuilder(new Pose2d(12, 62, Math.toRadians(270)))
-
-                                // BLUE LEFT (LEFT PROP)
-
-                                .strafeLeft(11.5)
-
-                                .waitSeconds(5)
-
-                                /**
-
-                                // BLUE LEFT (CENTER PROP)
-                                .lineToLinearHeading(new Pose2d(15, 24, Math.toRadians(180)))
-                                .setReversed(true)
-
-                                .splineToConstantHeading(new Vector2d(50, 59), Math.toRadians(0))
-                                .waitSeconds(1)
-
-                                .lineTo(new Vector2d(-36, 59))
-
-                                .splineTo(new Vector2d(-57, 36), Math.toRadians(180))
-                                .waitSeconds(1)
-
-                                // if passover this turn isn't needed
-
-                                .splineToConstantHeading(new Vector2d(-36, 59),Math.toRadians(0))
-
-                                .lineTo(new Vector2d(60,59))
-
-                                .waitSeconds(1)
-
-
-
-                                .build()
-
-
-
-                );
-**/
 
         meepMeep.setBackground(MeepMeep.Background.FIELD_CENTERSTAGE_JUICE_DARK)
                 .setDarkMode(true)
                 .setBackgroundAlpha(0.95f)
                 .addEntity(blueRight)
-                // .addEntity(blueLeft)
                 .start();
 
     }
