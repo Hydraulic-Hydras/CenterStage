b6a800142b6bfa42a4286c7dc2974595968dc4fc
Tea <mtauha9@gmail.com>
2023-11-09 22:07:04 -0500

    Constants

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CenterStage/AutoConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CenterStage/AutoConstants.java
@@ -9,20 +9,17 @@ import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
 
 @Config
 public class AutoConstants {
-    public enum Side {
-        BLUE,
-        RED
-    }
 
-    public static int LEFTSIDE_REGION_X = 0;
-    public static int LEFTSIDE_REGION_Y = 170;
+     /**
+      public static int LEFTSIDE_REGION_X = 0;
+      public static int LEFTSIDE_REGION_Y = 170;
 
-    public static int RIGHTSIDE_REGION_X = 0;
-    public static int RIGHTSIDE_REGION_Y = 170;
+      public static int RIGHTSIDE_REGION_X = 0;
+      public static int RIGHTSIDE_REGION_Y = 170;
 
-    public static int REGION_WIDTH = 65;
-    public static int REGION_HEIGHT = 65;
-
+      public static int REGION_WIDTH = 65;
+      public static int REGION_HEIGHT = 65;
+      **/
 
     // VELOCITY AND ACCELERATION
     public static final int MAX_VEL = 100;
@@ -32,36 +29,21 @@ public class AutoConstants {
     public static final int VELO = 58;
     public static final int ACCEL = 58;
 
-    public static final TrajectoryVelocityConstraint Vel0 = SampleMecanumDrive.getVelocityConstraint(FAST_VEL,
+    public static final TrajectoryVelocityConstraint Vel0 = SampleMecanumDrive.getVelocityConstraint
+            (FAST_VEL,
             Math.toRadians(180), Math.toRadians(180));
-    public static final TrajectoryAccelerationConstraint Accel0 = SampleMecanumDrive.getAccelerationConstraint(FAST_ACCEL);
+    public static final TrajectoryAccelerationConstraint Accel0 = SampleMecanumDrive.getAccelerationConstraint
+            (FAST_ACCEL);
 
 
-    // BLUE RIGHT START POSE
-    public static final int BR_X = -36;
-    public static final int BR_Y = 62;
-    public static final double BR_H = Math.toRadians(270);
-    public static final Pose2d BR_START = new Pose2d(BR_X, BR_Y, BR_H);
+    /* ======= COORDINATE CONSTANTS ======= */
+    public static final double HEADING = Math.toRadians(90);
+    public static final int LEFT_X = -35;
+    public static final double L_WALLPOS = -1 * (70.5 - (16.5 / 2.0));
+    public static final Pose2d LEFT_SP = new Pose2d(LEFT_X, L_WALLPOS, HEADING);
+    public static final int RIGHT_X = 12;
+    public static final double R_WALLPOS = 1 * (70.5 - (16.5 / 2.0));
+    public static final Pose2d RIGHT_SP = new Pose2d(RIGHT_X, R_WALLPOS, HEADING);
 
-    // BLUE LEFT START POSE
-    public static final int BL_X = 12;
-    public static final int BL_Y = 62;
-    public static final double BL_H = Math.toRadians(270);
-    public static final Pose2d BL_START = new Pose2d(BL_X, BL_Y, BL_H);
-
-    // RED RIGHT START POSE
-    public static final int RR_X = 12;
-    public static final int RR_Y = -62;
-    public static final double RR_H = Math.toRadians(180);
-    public static final Pose2d RR_START = new Pose2d(RR_X, RR_Y, RR_H);
-
-    // RED LEFT START POSE
-    public static final int RL_X = -36;
-    public static final int RL_Y = -62;
-    public static final double RL_H = Math.toRadians(180);
-    public static final Pose2d RL_START = new Pose2d(RL_X, RL_Y, RL_H);
-
-    public static Side SIDE = Side.RED;
-
-
+    /* ======= END COORDINATE CONSTANTS  ======= */
 }
