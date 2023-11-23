fcb3ee522d46fb103dae86129a85c629a9063f60
Tea <mtauha9@gmail.com>
2023-11-23 15:48:27 -0500

    MathUtils update

--- HydraulicLib/src/main/java/com/hydraulichydras/hydrauliclib/Geometry/MathUtils.java
+++ HydraulicLib/src/main/java/com/hydraulichydras/hydrauliclib/Geometry/MathUtils.java
@@ -1,5 +1,8 @@
 package com.hydraulichydras.hydrauliclib.Geometry;
 
+/**
+ * MathUtils is an utility class that contains various mathematical functions and operations.
+ */
 public class MathUtils {
 
     public static double norm(double angle){
