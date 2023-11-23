7c9a5f90f376d34384dcac2d366aa43372c4c063
Tea <mtauha9@gmail.com>
2023-11-23 15:47:35 -0500

    Vector2D update

--- HydraulicLib/src/main/java/com/hydraulichydras/hydrauliclib/Geometry/Vector2D.java
+++ HydraulicLib/src/main/java/com/hydraulichydras/hydrauliclib/Geometry/Vector2D.java
@@ -1,5 +1,9 @@
 package com.hydraulichydras.hydrauliclib.Geometry;
 
+/**
+ *  A vector in 2D space typically consists of two components: one along the x-axis and one along the y-axis.
+ *  This class is often used for geometric operations necessary.
+ */
 public class Vector2D {
 
     public double x,y;
