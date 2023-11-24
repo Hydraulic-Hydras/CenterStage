ff8954a4960d927c217a7b55411f4ea65b9b0ddf
Tea <mtauha9@gmail.com>
2023-11-23 20:38:33 -0500

    womp womp

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CenterStage/common/Vision/AprilTagDetectionPipeline.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CenterStage/common/Vision/AprilTagDetectionPipeline.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode.CenterStage.common.PipeLines;
+package org.firstinspires.ftc.teamcode.CenterStage.common.Vision;
 
 import org.opencv.calib3d.Calib3d;
 import org.opencv.core.CvType;
