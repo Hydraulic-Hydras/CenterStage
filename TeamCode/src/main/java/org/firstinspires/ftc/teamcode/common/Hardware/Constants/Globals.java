0cfb809ede4074613b0091c34ffedbe03cd1ba37
Tea505 <mtauha9@gmail.com>
2023-12-30 03:59:05 -0500

    Rewamp

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/Hardware/Constants/Globals.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/Hardware/Constants/Globals.java
@@ -5,7 +5,6 @@ import com.acmerobotics.roadrunner.geometry.Pose2d;
 import com.acmerobotics.roadrunner.geometry.Vector2d;
 import com.acmerobotics.roadrunner.trajectory.constraints.TrajectoryAccelerationConstraint;
 import com.acmerobotics.roadrunner.trajectory.constraints.TrajectoryVelocityConstraint;
-import com.hydraulichydras.hydrauliclib.Geometry.Pose;
 
 import org.firstinspires.ftc.teamcode.Tuning.SampleMecanumDrive;
 
