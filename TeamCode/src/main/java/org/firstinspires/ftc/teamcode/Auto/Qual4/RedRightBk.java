510d145584181346f0a8cab76852663813b0b6a7
Tea505 <mtauha9@gmail.com>
2024-01-30 23:08:47 -0500

    fine tuning

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/Qual4/RedRightBk.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/Qual4/RedRightBk.java
@@ -4,7 +4,6 @@ import com.acmerobotics.roadrunner.geometry.Pose2d;
 import com.acmerobotics.roadrunner.geometry.Vector2d;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.robotcore.external.JavaUtil;
 import org.firstinspires.ftc.robotcore.external.hardware.camera.BuiltinCameraDirection;
